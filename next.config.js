9a631bfd1b33c2a9aca570a7f940f91fef8d5151
Abbasian.dev <mam-1371@outlook.com>
2025-12-06 21:33:27 +0330

    hero video compressed

--- next.config.js
+++ next.config.js
@@ -1,11 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['localhost'],
-    formats: ['image/webp', 'image/avif'],
-  },
-  compress: true,
-  poweredByHeader: false,
+    images: {
+        domains: ['localhost'],
+        formats: ['image/webp', 'image/avif'],
+    },
+    compress: true,
+    poweredByHeader: false,
+
+    webpack: (config, { isServer }) => {
+        config.module.rules.push({
+            test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)$/,
+            type: 'asset/resource',
+            generator: {
+                filename: 'static/media/[name].[hash][ext]',
+            },
+        })
+
+        return config
+    },
+
+    experimental: {
+        optimizePackageImports: ['lucide-react'],
+    },
 }
 
 module.exports = nextConfig
